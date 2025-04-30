a749d81631ca118bc3e49ece3b3500c58225fdd1
VanshajR <vanshajraghuvanshi@gmail.com>
2025-04-30 22:09:00 +0530

    port updated

--- main.py
+++ main.py
@@ -90,4 +90,4 @@ async def chat(req: ChatRequest):
     
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
